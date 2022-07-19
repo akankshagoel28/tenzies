66bc2e1af27f055365dc291988f7abc05f388b86
Megha Maitin <megha.maitin2020@vitstudent.ac.in>
2022-07-19 23:38:28 +0530

    Megha the saviour

--- src/App.js
+++ src/App.js
@@ -1,48 +1,59 @@
 import React from "react"
 import Die from "./Die"
-import {nanoid} from "nanoid"
+import { nanoid } from "nanoid"
 import Confetti from "react-confetti"
+import { useRef,useState} from "react";
 
 export default function App() {
-
+    const [start, setStart] = useState(true);
     const [dice, setDice] = React.useState(allNewDice())
     const [tenzies, setTenzies] = React.useState(false)
-    const [rolls, setRolls]=React.useState(0)
-    const [seconds,setSeconds]=React.useState(0)
-    const [minutes,setMinutes]=React.useState(0)
-    const [highscore, setHighScore] = React.useState(() => {
-        const saved = localStorage.getItem("highscore");
-        const initialValue = JSON.parse(saved);
-        return initialValue || "";
-      });
-    var timer;
+    const [rolls, setRolls] = React.useState(0)
+    const [seconds, setSeconds] = React.useState(0)
+    const [minutes, setMinutes] = React.useState(0)
+    const [highscore, setHighScore] = React.useState(0)
+    //  => {
+    //     const saved = localStorage.getItem("highscore");
+    //     const initialValue = JSON.parse(saved);
+    //     return initialValue || "";
+    // });
+    const timer = useRef();
+    // var flag = 0;
+    // React.useEffect(() => {
+    //     if (timer < highscore) {
+    //         localStorage.setItem("highscore", JSON.stringify(highscore));
+    //         console.log(highscore);
+    //         setHighScore(timer)
+    //     }
+    // }, [highscore]);
     React.useEffect(() => {
-        if(time<highscore){
-        localStorage.setItem("highscore", JSON.stringify(highscore));
-        setHighScore(time)
+        if (start) {
+            timer.current = setInterval(() => {
+                setSeconds((seconds) => seconds + 1)
+                if (seconds === 59) {
+                    setSeconds(0)
+                    setMinutes(minutes + 1)
+                }
+            }, 1000);
         }
-      }, [highscore]);
-    React.useEffect(() => {
-        timer = setInterval(() =>{
-        setSeconds(seconds + 1)
-        if(seconds === 59)
-        {
-            setSeconds(0)
-            setMinutes(minutes+1)
+        else {
+            console.log("clear interval");
+            clearInterval(timer.current);
         }
-    },1000)
-    return ()=> clearInterval(timer);
+        return () => clearInterval(timer.current);
 
-})
-    var time;
+    });
+    // var time;
     React.useEffect(() => {
         const allHeld = dice.every(die => die.isHeld)
         const firstValue = dice[0].value
         const allSameValue = dice.every(die => die.value === firstValue)
         if (allHeld && allSameValue) {
-            setTenzies(true)
-            clearInterval(timer)
-            time=timer
+            setTenzies(true);
+            console.log(seconds);
+            setStart(!start);
+            setHighScore(minutes*60+seconds);
+            localStorage.getItem("highscore",highscore);
         }
     }, [dice])
 
@@ -53,7 +64,7 @@ export default function App() {
             id: nanoid()
         }
     }
-    
+
     function allNewDice() {
         const newDice = []
         for (let i = 0; i < 10; i++) {
@@ -61,13 +72,13 @@ export default function App() {
         }
         return newDice
     }
-    function rollNo(){
-        setRolls(oldno => oldno+1)
+    function rollNo() {
+        setRolls(oldno => oldno + 1)
     }
     function rollDice() {
-        if(!tenzies) {
+        if (!tenzies) {
             setDice(oldDice => oldDice.map(die => {
-                return die.isHeld ? 
+                return die.isHeld ?
                     die :
                     generateNewDie()
             }))
@@ -79,42 +90,42 @@ export default function App() {
             setMinutes(0)
         }
     }
-    
+
     function holdDice(id) {
         setDice(oldDice => oldDice.map(die => {
-            return die.id === id ? 
-                {...die, isHeld: !die.isHeld} :
+            return die.id === id ?
+                { ...die, isHeld: !die.isHeld } :
                 die
         }))
     }
-    
+
     const diceElements = dice.map(die => (
-        <Die 
-            key={die.id} 
-            value={die.value} 
-            isHeld={die.isHeld} 
+        <Die
+            key={die.id}
+            value={die.value}
+            isHeld={die.isHeld}
             holdDice={() => holdDice(die.id)}
         />
     ))
-    
+
     return (
         <main>
             {tenzies && <Confetti />}
             <h1 className="title">Tenzies</h1>
-            <p className="instructions">Roll until all dice are the same. 
-            Click each die to freeze it at its current value between rolls.</p>
+            <p className="instructions">Roll until all dice are the same.
+                Click each die to freeze it at its current value between rolls.</p>
             <div className="dice-container">
                 {diceElements}
             </div>
-            <button 
-                className="roll-dice" 
-                onClick={function(event){rollDice(); rollNo()}}
+            <button
+                className="roll-dice"
+                onClick={function (event) { rollDice(); rollNo() }}
             >
                 {tenzies ? "New Game" : "Roll"}
             </button>
             <div className="extras"><p>Rolls: {rolls}</p>
-            <p>Time: {minutes>0?minutes+"m ":""}{seconds<10?seconds +"s":seconds+"s"}</p>
-            <h5 style={{margin:"8px 8px",color:"red"}}>Least time: {highscore?"0":highscore}</h5></div>
+                <p>Time: {minutes > 0 ? minutes + "m " : ""}{seconds < 10 ? seconds + "s" : seconds + "s"}</p>
+                <h5 style={{ margin: "8px 8px", color: "red" }}>Least time: {highscore>60? Math.floor(highscore/60)+"m"+" "+highscore%60+"s":highscore+"s"}</h5></div>
         </main>
     )
 }
